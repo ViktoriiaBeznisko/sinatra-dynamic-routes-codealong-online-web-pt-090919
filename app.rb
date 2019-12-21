e3d99c93f399d80db9be910e0167afe078488666
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-12-21 05:04:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,8 +21,9 @@ class App < Sinatra::Base
   end
 
   get "multiply/:num1/:num2" do
-    @user_name = params[:name]
-    "Goodbye, #{@user_name}."
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    "#{@num1 * @num2}"
   end
 
 end
