920fc4e0924d3f1ce800fff19a0b2811d953999e
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-12-21 04:52:45 +0000

    Automatically backed up by Learn

--- medicines_controller.rb
+++ medicines_controller.rb
@@ -1,6 +1,6 @@
-get '/medicines/:id' do
+get '/goodbye/:name'/goodbye/:' do
   @medicine = all_the_medicines.select do |medicine|
     medicine.id == params[:id]
   end.first
-  erb :'/medicines/show.html'
+  erb :'/goodbye/show.html'
 end
